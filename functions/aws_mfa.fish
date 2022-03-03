9f8ac03ae992a6c49ff394986126cc1444f31710
Frederick Price <rprice@pricemail.ca>
2022-03-03 10:16:12 -0500

    Try to fix things so that it won't run when fish shell isn't interactive

--- functions/aws_mfa.fish
+++ functions/aws_mfa.fish
@@ -2,7 +2,8 @@ function aws_mfa -d "Run AWS mfa if needed"
 
     aws s3 ls > /dev/null 2>&1
 
-    if test $status -ne 0
+    # Don't run when not interactive because user can't enter mfa token anyway and it breaks stuff
+    if test $status -ne 0 and not fish -c 'status --is-interactive'
         pushd .
         cd $THR_ROOT && bazel run //extras/aws-mfa-auth
         popd
