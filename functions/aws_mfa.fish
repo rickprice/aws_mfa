3bb0b9ed952f55cf11410d629e1c8055070ca57e
Frederick Price <rprice@pricemail.ca>
2022-03-03 10:44:59 -0500

    Change so that it's faster for not interactive

--- functions/aws_mfa.fish
+++ functions/aws_mfa.fish
@@ -1,11 +1,11 @@
 function aws_mfa -d "Run AWS mfa if needed"
 
-    aws s3 ls > /dev/null 2>&1
-
     # Don't run when not interactive because user can't enter mfa token anyway and it breaks stuff
-    if test $status -ne 0 
-        status --is-interactive
-        if test $status -eq 0
+    status --is-interactive
+    if test $status -eq 0
+
+        aws s3 ls > /dev/null 2>&1
+        if test $status -ne 0 
             pushd .
             cd $THR_ROOT && bazel run //extras/aws-mfa-auth
             popd
