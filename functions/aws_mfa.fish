0a3ddab119ab0071a9bb66cbe25e788bee199315
Frederick Price <rprice@pricemail.ca>
2022-03-03 10:27:32 -0500

    Try again

--- functions/aws_mfa.fish
+++ functions/aws_mfa.fish
@@ -3,7 +3,7 @@ function aws_mfa -d "Run AWS mfa if needed"
     aws s3 ls > /dev/null 2>&1
 
     # Don't run when not interactive because user can't enter mfa token anyway and it breaks stuff
-    if test $status -ne 0 and not fish -c 'status --is-interactive'
+    if test $status -ne 0 and not test status --is-interactive
         pushd .
         cd $THR_ROOT && bazel run //extras/aws-mfa-auth
         popd
